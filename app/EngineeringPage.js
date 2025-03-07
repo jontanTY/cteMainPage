c2250e448b711509075112392bc7a547a07c3178
swagfather808 <rkringor@gmail.com>
2025-03-07 09:05:49 -1000

    Revert "Revert "Classes,etc""

---
+++ app/EngineeringPage.js
@@ -0,0 +1,11 @@
+import { Text, View } from "react-native-web";
+import { Link } from "expo-router";
+
+export default function EngineeringPage() {
+    return (
+        <View>
+            <Text>Engineering Page</Text>
+            <Link href='/'>Go back home</Link>
+        </View>
+    )
+}
