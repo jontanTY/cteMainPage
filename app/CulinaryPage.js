dc43d30db3d0072eaec5bb43c82c9dedcf38689c
DanielY07 <juniorwot46@gmail.com>
2025-03-28 09:45:17 -1000

    added video to culinary

--- app/CulinaryPage.js
+++ app/CulinaryPage.js
@@ -2,7 +2,7 @@ import { Text, View } from "react-native-web";
 import { Link } from "expo-router";
 import { ScrollView } from 'react-native';
 import PageTemplate from "../components/PageTemplate";
-import { VideoView, useVideoPlayer } from 'expo-video'
+import { VideoView, useVideoPlayer } from 'expo-video';
 
 export default function CulinaryPage() {
     const player = useVideoPlayer(require('../assets/culinaryvid.mp4'), player =>  { player.play() }); // This will play the video when the component mounts. You can also call player.pause() to pause it or player.seek(0) to seek to the beginning.
