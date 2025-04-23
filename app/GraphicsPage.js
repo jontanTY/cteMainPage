5c78d178695482973ed84ef9c1750d8966b20944
swagfather808 <rkringor@gmail.com>
2025-04-23 09:23:55 -1000

    Set up graphics page and import graphics page

---
+++ app/GraphicsPage.js
@@ -0,0 +1,11 @@
+import { Text, View } from "react-native-web";
+import { Link } from "expo-router";
+import PageTemplate from "../components/PageTemplate";
+
+export default function GraphicsPage() {
+    return (
+        <View>
+            <PageTemplate />
+        </View>
+    )
+}
